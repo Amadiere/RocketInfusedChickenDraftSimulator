937a62b31dac248b2e32dac6450aa6ab040530e6
Unknown <alex.holt@webuyanycar.com>
2018-11-07 20:17:35 +0000

    Initial project creation.

---
+++ RocketInfusedChickenDraftSimulator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RocketInfusedChickenDraftSimulator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
