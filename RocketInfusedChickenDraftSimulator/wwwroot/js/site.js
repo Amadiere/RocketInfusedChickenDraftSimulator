43bead92254eac51a8766b9af44b032c5d69d396
Unknown <alex.holt@webuyanycar.com>
2018-12-06 22:03:41 +0000

    Updated interface
    Now have extra clickiness and Previews

--- RocketInfusedChickenDraftSimulator/wwwroot/js/site.js
+++ RocketInfusedChickenDraftSimulator/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿(function RocketInfused() {
 
-// Write your JavaScript code.
+    getRavnicaBooster();
+    var allCards = document.getElementById('current-pack').getElementsByClassName('mtg-card');
+
+    for (var i = 0; i < allCards.length; i++) {
+        allCards[i].addEventListener('mouseover', onHover);
+        allCards[i].addEventListener('click', onClick);
+    }
+
+    function onHover(event) {
+        document.getElementsByClassName('mtg-preview')[0].src = this.src;
+    }
+
+    function onClick(event) {
+        var cardList = document.getElementById('user-deck').getElementsByClassName('card-list')[0];
+        this.removeEventListener('click', onClick);
+        cardList.appendChild(this);
+    }
+
+    function getRavnicaBooster() {
+        var url = 'http://api.magicthegathering.io/v1/sets/GRN/booster';
+
+        //fetch(url, { 
+        //    method: 'GET', 
+        //    headers: { 'content-type': 'application/json' } 
+        //})
+        //    .then(function (response) {
+        //        console.log(response)
+        //    })
+
+        //var xmlHttp = new XMLHttpRequest();
+        //xmlHttp.open('GET', url, false);
+        //xmlHttp.withCredentials = true;
+        //xmlHttp.setRequestHeader("Content-Type", "application/json");
+        //xmlHttp.send(null);
+
+        //var result = xmlHttp.responseText;
+    }
+}());
